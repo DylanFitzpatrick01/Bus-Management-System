31c659c1c01654f5528a1ea4c392ca92f92220c0
fitzpady <fitzpady@tcd.ie>
2022-03-26 21:36:25 +0000

    add main class

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Main class which the user interacts with
+ */
+public class Main {
+    public static void main(String[] args){
+        // initialise Scanner to allow user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Vancouver Bus Management System! \n");
+    }
+}
